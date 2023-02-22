e3ffc332186f21facca80ddc954640cfaf650a19
HelioAmaral <helio.amaral81@gmail.com>
2023-02-22 12:42:51 +0100

    modal update

--- src/app/mods/modal/modal.component.ts
+++ src/app/mods/modal/modal.component.ts
@@ -1,18 +1,33 @@
-import { Component, OnInit, ElementRef } from '@angular/core';
+//import Output and EventEmitter to do child to parent communication
+import {
+  Component,
+  OnInit,
+  ElementRef,
+  Output,
+  EventEmitter,
+} from '@angular/core';
 
 @Component({
   selector: 'app-modal',
   templateUrl: './modal.component.html',
-  styleUrls: ['./modal.component.css']
+  styleUrls: ['./modal.component.css'],
 })
 export class ModalComponent implements OnInit {
+  //output decorator to do child to parent communication, close property is the event we are going to trigger, thats the event we are going to watch for in the parent component, this will be an instance of a new EventEmitter
+  @Output() close = new EventEmitter();
 
-  constructor(private el: ElementRef){
-
-  }
-//ngOnInit is a lifeCycle hook (a method)
+  constructor(private el: ElementRef) {}
+  //ngOnInit is a lifeCycle hook (a method)
   ngOnInit() {
-    document.body.appendChild(this.el.nativeElement); 
+    document.body.appendChild(this.el.nativeElement);
   }
 
+  ngOnDestroy() {
+    this.el.nativeElement.remove();
+  }
+
+  //onCloseClick method, anytime we call onCloseClick we will refference the close emmiter and emitt an event
+  onCloseClick() {
+    this.close.emit();
+  }
 }
--- src/app/mods/mods-home/mods-home.component.ts
+++ src/app/mods/mods-home/mods-home.component.ts
@@ -7,4 +7,12 @@ import { Component } from '@angular/core';
 })
 export class ModsHomeComponent {
 
+  //property that will control if the modal is shown or not
+  modalOpen = false;
+
+  //onClick method will revert the modalOpen property, if its false turns it into true and vice versa
+  onClick(){
+this.modalOpen = !this.modalOpen;
+  }
+
 }
